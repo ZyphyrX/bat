c92833488efb734c33a3b350ac94188fb2fbffe6
ZyphyrX <121889498+ZyphyrX@users.noreply.github.com>
2024-03-12 12:30:20 +0530

    Add files via upload

---
+++ tor.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting Tor Proxy...
+cd "C:\Users\black\Desktop\Tor Browser\Browser\TorBrowser\Tor"
+start tor.exe
+echo Tor Proxy started successfully.
+echo Press any key to exit...
+pause >nul
